201e19ddf7e93ba4f638964b56dba27848911cb7
navitoyui <41223115l@gapps.ntnu.edu.tw>
2025-05-30 21:16:08 +0800

    Update exhibitions.js

--- exhibitions.js
+++ exhibitions.js
@@ -86,13 +86,14 @@ function renderExhibitionCards(exhibitions) {
                 }
 
                 // 建立紅色 marker
-                const redIcon = L.icon({
-                    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x-red.png",
+                const greenIcon = L.icon({
+                    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
                     iconSize: [25, 41],
                     iconAnchor: [12, 41],
                     popupAnchor: [1, -34],
                     shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
                     shadowSize: [41, 41]
+                    className: 'leaflet-marker-green'
                 });
 
                 exhibitionHighlightMarker = L.marker([lat, lng], { icon: redIcon })
